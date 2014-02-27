b596a6fe3a044fc8e6c293bebbf486e735af80db
Leandro Brunner <leandrobrunner@yahoo.com.ar>
2014-02-27 19:58:11 -0300

    - Agregada la página "Quienes somos", "Miembros de PyAr", "Lista de correo". - Agregada la classe "http" a los links. (Para los links que salen se la web).

--- pyarweb/views.py
+++ pyarweb/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def irc(request):
     return render(request, 'irc/irc.html')
+
+def quienes_somos(request):
+    return render(request, 'quienes_somos.html')
+    
+def MiembrosDePyAr(request):
+    return render(request, 'MiembrosDePyAr.html')
+    
+def ListaDeCorreo(request):
+    return render(request, 'ListaDeCorreo.html')
