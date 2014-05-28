4b5f4fdedd66f5a5a28c993cdf4aa6c7e459d00b
Martín Gaitán <gaitan@gmail.com>
2014-05-28 00:10:42 -0300

    wtf with statics?¡

--- community/views.py
+++ community/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def homepage(request):
+    return render(request, 'community/index.html')
--- pyarweb/settings.py
+++ pyarweb/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
