3301c919ea2f6c57ca87d6e5e03eb256631808dd
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2014-11-13 15:07:25 -0300

    fix de view de company

--- pycompanies/urls.py
+++ pycompanies/urls.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import patterns, url
+from django.views.generic.detail import DetailView
+from pycompanies.models import Company
 from .views import (
     add,
     list_all,
@@ -10,7 +12,10 @@ from .views import (
 
 urlpatterns = patterns('',
     url(r'^$', list_all, name='companies_list_all'),
-    url(r'^add$', add),
+    url(r'^add$', add, name='companies-add'),
     url(r'^edit/$', edit),
     url(r'^(?P<company_id>\d+)/edit$', edit),
+    url(r'^(?P<pk>\d+)/$',
+        DetailView.as_view(model=Company),
+        name='company_view'),
 )
