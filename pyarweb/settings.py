0ae4d84415d18af87445b3c536756fc4d2facc51
Cesar H. Roldan <croldan@gmail.com>
2014-11-19 11:19:06 -0300

    Added custom Date and Time formats for es_AR locale

--- pyarweb/settings.py
+++ pyarweb/settings.py
@@ -141,6 +141,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+FORMAT_MODULE_PATH = 'pyarweb.formats'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
