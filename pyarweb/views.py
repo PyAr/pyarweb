0f277d74e6752557f25eed2fb8c3c740b29e1fde
Martín Gaitán <gaitan@gmail.com>
2014-11-24 19:30:43 -0300

    fix waliki redirection

--- pyarweb/views.py
+++ pyarweb/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from waliki.models import Page
+from django.http import Http404
+from django.shortcuts import render, redirect
+from waliki.models import Page, Redirect
 
 
 def irc(request):
     return render(request, 'irc/irc.html')
 
+
 def special_page(request, **kwargs):
     return render(request, 'special_page.html', kwargs)
+
 
 def buscador(request):
     return render(request, 'buscador.html', {'buscar': request.GET.get('buscar', '')})
 
+
 def old_url_redirect(request, slug):
-    page = Page.objects.get(slug=slug)
+
+    try:
+        redirect = Redirect.objects.get(old_slug=slug)
+        slug = redirect.new_slug
+    except Redirect.DoesNotExist:
+        pass
+
+    try:
+        page = Page.objects.get(slug=slug)
+    except Page.DoesNotExist:
+        raise Http404
+
     return redirect(page.get_absolute_url(), permanent=True)
