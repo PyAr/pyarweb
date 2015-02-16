2964a70bcb60bee5325cde63baace75a9c8b6b24
surrealcristian <surrealcristian@gmail.com>
2015-02-16 00:27:17 -0300

    #210 Rename de los métodos <get_query_set> a <get_queryset> (debido a RemovedInDjango18Warning).

--- planet/managers.py
+++ planet/managers.py
@@ -7,62 +7,62 @@ from django.conf import settings
 class FeedManager(models.Manager):
     """
     """
-    def get_query_set(self):
-        qs = super(FeedManager, self).get_query_set()
+    def get_queryset(self):
+        qs = super(FeedManager, self).get_queryset()
         return qs.filter(site=settings.SITE_ID, is_active=True)
 
 
 class FeedLinkManager(models.Manager):
     """
     """
-    def get_query_set(self):
-        qs = super(FeedLinkManager, self).get_query_set()
+    def get_queryset(self):
+        qs = super(FeedLinkManager, self).get_queryset()
         return qs.filter(feed__site=settings.SITE_ID)
 
 
 class BlogManager(models.Manager):
     """
     """
-    def get_query_set(self):
-        qs = super(BlogManager, self).get_query_set()
+    def get_queryset(self):
+        qs = super(BlogManager, self).get_queryset()
         return qs.filter(feed__site=settings.SITE_ID).distinct()
 
 
 class GeneratorManager(models.Manager):
     """
     """
-    def get_query_set(self):
-        qs = super(GeneratorManager, self).get_query_set()
+    def get_queryset(self):
+        qs = super(GeneratorManager, self).get_queryset()
         return qs.filter(feed__site=settings.SITE_ID).distinct()
 
 
 class AuthorManager(models.Manager):
     """
     """
-    def get_query_set(self):
-        qs = super(AuthorManager, self).get_query_set()
+    def get_queryset(self):
+        qs = super(AuthorManager, self).get_queryset()
         return qs.filter(post__feed__site=settings.SITE_ID).distinct()
 
 
 class PostManager(models.Manager):
     """
     """
-    def get_query_set(self):
-        qs = super(PostManager, self).get_query_set()
+    def get_queryset(self):
+        qs = super(PostManager, self).get_queryset()
         return qs.filter(feed__site=settings.SITE_ID).distinct()
 
 
 class PostLinkManager(models.Manager):
     """
     """
-    def get_query_set(self):
-        qs = super(PostLinkManager, self).get_query_set()
+    def get_queryset(self):
+        qs = super(PostLinkManager, self).get_queryset()
         return qs.filter(post__feed__site=settings.SITE_ID).distinct()
 
 
 class EnclosureManager(models.Manager):
     """
     """
-    def get_query_set(self):
-        qs = super(EnclosureManager, self).get_query_set()
+    def get_queryset(self):
+        qs = super(EnclosureManager, self).get_queryset()
         return qs.filter(post__feed__site=settings.SITE_ID).distinct()
