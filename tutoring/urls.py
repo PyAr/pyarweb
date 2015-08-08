e33524e8c8bdcff14e78a8995db1e83c5365e1c5
ScorpMarv <marv1336@gmail.com>
2015-08-08 19:10:59 -0300

    Completed all views for Mentor

--- tutoring/urls.py
+++ tutoring/urls.py
@@ -4,7 +4,9 @@ from tutoring import views
 
 urlpatterns = patterns('',
     url(r'^mentor/add/$', login_required(views.AddMentor.as_view()), name='new_mentor'),
-    url(r'^mentor/(?P<pk>[0-9]+)/$', login_required(views.UpdateMentor.as_view()), name='update_mentor'),
+    url(r'^mentor/update/(?P<pk>[0-9]+)/$', login_required(views.UpdateMentor.as_view()), name='update_mentor'),
+    url(r'^mentor/delete/(?P<pk>[0-9]+)/$', login_required(views.DeleteMentor.as_view()), name='delete_mentor'),
+    url(r'^mentor/(?P<pk>[0-9]+)/$', views.DisplayMentor.as_view(), name='display_mentor'),
     url(r'^apprentice/add/$', views.AddApprentice.as_view(), name='new_apprentice'),
     url(r'^project/add/$', views.AddProject.as_view(), name='new_project'),
     # url(r'^(?P<tutoring_id>\d+)/$', views.detail, name='detail'),
--- tutoring/views.py
+++ tutoring/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
 from .models import Project, Mentor, Apprentice, Project
 from .forms import MentorForm, ApprenticeForm, ProjectForm
@@ -16,6 +18,21 @@ def index(request):
 #         form.instance.owner = self.request.user
 #         return super(JobCreate, self).form_valid(form)
 
+
+class DisplayMentor(DetailView):
+    model = Mentor
+    form_class = MentorForm
+
+    def get_context_data(self, **kwargs):
+        context = super(DisplayMentor, self).get_context_data(**kwargs)
+        return context
+
+
+class DeleteMentor(DeleteView):
+    model = Mentor
+    success_url = reverse_lazy('new_mentor')
+
+
 class AddMentor(CreateView):
     model = Mentor
     form_class = MentorForm
@@ -25,9 +42,11 @@ class AddMentor(CreateView):
         form.instance.user = self.request.user
         return super(AddMentor, self).form_valid(form)
 
+
 class UpdateMentor(UpdateView):
     model = Mentor
     form_class = MentorForm
+
 
 class AddApprentice(CreateView):
     model = Apprentice
@@ -36,6 +55,7 @@ class AddApprentice(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddApprentice, self).form_valid(form)
+
 
 class AddProject(CreateView):
     model = Project
