900238c94f2301cf6dc2ea5bcafedd5246fa7caf
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2016-05-07 17:20:25 -0300

    webcli, nuestro nuevo superscript

--- scripts/webcli.py
+++ scripts/webcli.py
@@ -6,49 +6,59 @@ import argparse
 import subprocess
 
 
+CONTAINER_NAME = 'pyar'
+
+
 parser = argparse.ArgumentParser(
     prog='webcmd',
     description='Helper to start a Pyarweb development instance.')
 
 parser.add_argument(
-    '--docker-run',
+    '--build-image',
     action='store_true',
-    help='Run command: docker run -it -v ...')
+    help='Build PyAr web docker image')
+
+parser.add_argument(
+    '--run-container',
+    action='store_true',
+    help='Build PyAr web docker image')
+
+parser.add_argument(
+    '--run-shell',
+    action='store_true',
+    help='Exec a bash interpreter on a running container')
 
 
-def check_dockerfile():
-    """Check if Dockerfile exists in current path """
+def build():
+    """Run docker build, then docker run."""
     curdir = os.path.abspath(os.path.curdir)
     dockerfile = os.path.join(curdir, 'Dockerfile')
     assert os.path.isfile(dockerfile), "{} file not found.".format(dockerfile)
 
-
-def docker_run():
-    """Run docker build, then docker run."""
-    check_dockerfile()
     cmd = "docker build -t pyarweb/django ."
     subprocess.run(shlex.split(cmd))
-    cmd = "docker run -it -v {}:/opt/code -p 8000:8000 --name pyar pyarweb/django".format(curdir)
-    completed_process = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stderr = completed_process.stderr
-    if bytes('Conflict. The name "/pyar" is already in use by container', 'utf-8') in stderr:
-        # Lets try again a second time, in a very,... slowly,... dirty,... way
-        # Remove docker container
-        cmd = "docker rm pyar"
-        subprocess.run(shlex.split(cmd))
-        # Try again
-        cmd = "docker run -it -v {}:/opt/code -p 8000:8000 --name pyar pyarweb/django".format(curdir)
-        print(cmd)
-        # This time, if it fails, drop to the terminal showing the error
-        subprocess.run(shlex.split(cmd))
 
 
-def docker_shell():
+def run():
+    """Run docker run to create a container from pyarweb/django image."""
+    curdir = os.path.abspath(os.path.curdir)
+    dockerfile = os.path.join(curdir, 'Dockerfile')
+    assert os.path.isfile(dockerfile), "{} file not found.".format(dockerfile)
+
+    cmd = "docker run -it -v {}:/opt/code -p 8000:8000 --name {} pyarweb/django".format(curdir, CONTAINER_NAME)
+    subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+def shell():
     """Get a bash shell in docker container."""
-    cmd = "docker exec -it pyar /bin/bash"
+    cmd = "docker exec -it {} /bin/bash".format(CONTAINER_NAME)
     subprocess.run(shlex.split(cmd))
 
 
 args = parser.parse_args()
-if args.docker_run:
-    docker_run()
+if args.build_image:
+    build()
+elif args.run_container:
+    run()
+elif args.run_shell:
+    shell()
