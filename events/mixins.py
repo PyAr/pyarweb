b0be50d417d7a327739c04e55f5f0713930d3c16
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2016-09-10 19:15:53 -0300

    adds a CrispyFormMixin helper and an EventParticipation Form

--- events/mixins.py
+++ events/mixins.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Reset, Layout, Div, Field
+from crispy_forms.helper import FormHelper
+from django import forms
 from django.core.urlresolvers import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Event
 
@@ -12,3 +17,21 @@ class EventMixin(object):
 
     def get_success_url(self):
         return reverse_lazy('events:events_list_all')
+
+
+
+class CrispyFormMixin(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.form_method = "post"
+        self.helper.add_input(Submit('job_submit', _('Guardar')))
+        self.helper.add_input(Reset('job_reset', _('Limpiar'),
+                              css_class='btn-default'))
+        self.helper.layout = Layout(
+            Div(*self.Meta.crispy_fields)
+        )
+
+
+    class Meta:
+        crispy_fields = []
