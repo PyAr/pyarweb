171f1caf18b0646e4d6e0a40bab79aed9788034a
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2016-09-10 19:17:15 -0300

    adds a participation registration url

--- events/urls.py
+++ events/urls.py
@@ -6,13 +6,17 @@ from .views import (EventDetail,
                     EventCreate,
                     EventUpdate,
                     EventDelete,
-                    EventsFeed)
+                    EventsFeed,
+                    EventParticipationCreate)
 
-urlpatterns = patterns('',
-                       url(r'^$', EventList.as_view(), name='events_list_all'),
-                       url(r'^rss$', EventsFeed(), name='events_feed'),
-                       url(r'^(?P<pk>\d+)/$', EventDetail.as_view(), name='detail'),
-                       url(r'^add/$', EventCreate.as_view(), name='add'),
-                       url(r'^(?P<pk>\d+)/edit/$', EventUpdate.as_view(), name='edit'),
-                       url(r'^(?P<pk>\d+)/delete/$', EventDelete.as_view(), name='delete'),
-                       )
+urlpatterns = patterns(
+	'',
+   url(r'^$', EventList.as_view(), name='events_list_all'),
+   url(r'^rss$', EventsFeed(), name='events_feed'),
+   url(r'^(?P<pk>\d+)/$', EventDetail.as_view(), name='detail'),
+   url(r'^add/$', EventCreate.as_view(), name='add'),
+   url(r'^(?P<pk>\d+)/edit/$', EventUpdate.as_view(), name='edit'),
+   url(r'^(?P<pk>\d+)/delete/$', EventDelete.as_view(), name='delete'),
+   url(r'^(?P<pk>\d+)/participate/$', EventParticipationCreate.as_view(),
+       name='participate')
+)
