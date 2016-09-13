bc11f310912e8c36876f499a6a9cb6f5824f64d0
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2016-09-12 23:22:21 -0300

    adds a ReadOnlyFieldsMixin

--- events/mixins.py
+++ events/mixins.py
@@ -10,17 +10,9 @@ from django.utils.translation import ugettext_lazy as _
 from .models import Event
 
 
-class EventMixin(object):
-    """Mixin for common attrs."""
-
-    model = Event
-
-    def get_success_url(self):
-        return reverse_lazy('events:events_list_all')
-
-
-
 class CrispyFormMixin(forms.ModelForm):
+    """Form with Guardar and Limpiar buttons, based in crispy forms."""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
@@ -32,6 +24,32 @@ class CrispyFormMixin(forms.ModelForm):
             Div(*self.Meta.crispy_fields)
         )
 
-
     class Meta:
         crispy_fields = []
+
+
+class EventMixin(object):
+    """Mixin for common attrs."""
+
+    model = Event
+
+    def get_success_url(self):
+        return reverse_lazy('events:events_list_all')
+
+
+class ReadOnlyFieldsMixin(object):
+    """Adds the posibility to declare fields as read-only (until we update to Django 1.9 or so"""
+
+    readonly_fields = ()
+
+    def __init__(self, *args, **kwargs):
+        super(ReadOnlyFieldsMixin, self).__init__(*args, **kwargs)
+        for field in (field for name, field in self.fields.items() if
+                      name in self.readonly_fields):
+            field.widget.attrs['disabled'] = 'true'
+            field.required = False
+
+    def clean(self):
+        for f in self.readonly_fields:
+            self.cleaned_data.pop(f, None)
+        return super(ReadOnlyFieldsMixin, self).clean()
