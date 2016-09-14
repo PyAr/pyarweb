ec13a0b8f52d1285f92d5a253ca14592af224e54
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2016-09-14 00:40:15 -0300

    adds email verification to non-authenticated EventParticipation requests

--- events/models.py
+++ events/models.py
@@ -1,7 +1,11 @@
+from django.db.models.signals import post_save
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
+from email_confirm_la.models import EmailConfirmation
 
 from jobs.models import JOB_SENIORITIES
 
@@ -52,6 +56,9 @@ class EventParticipation(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    email_confirmations = GenericRelation('email_confirm_la.EmailConfirmation',
+                                          content_type_field='content_type',
+                                          object_id_field='object_id')
 
     class Meta:
         unique_together = ("event", "email")
@@ -61,3 +68,27 @@ class EventParticipation(models.Model):
         if self.event:
             result += " in %s" % self.event.name
         return result
+
+    def verify_email(self):
+        """Start the email-verification process with this instance's email attribute."""
+
+        EmailConfirmation.objects.verify_email_for_object(
+            email=self.email,
+            content_object=self,
+            email_field_name='email'
+        )
+
+    def is_verified(self):
+        is_a_pyar_user = self.user is not None
+        email_was_verified = not self.email_confirmations.exists()
+        return is_a_pyar_user or email_was_verified
+
+
+@receiver(post_save, sender=EventParticipation)
+def post_anonymous_participation_creation(sender, instance, created, **kwargs):
+    """After an EventParticipation is created, if it's anonymous (not a PyAr user), start the
+       email-verification process.
+    """
+    if created and instance.user is None:
+        instance.verify_email()
+
