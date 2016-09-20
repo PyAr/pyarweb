24e6fc148bb8d50b737268f9ac81277eaf8e1ddf
Carlos Matías de la Torre <cmdelatorre@gmail.com>
2016-09-20 00:49:25 -0300

    updates forms to show sponsor related fields when needed. Fix registration for logged-in users

--- events/forms.py
+++ events/forms.py
@@ -1,4 +1,5 @@
 from captcha.fields import CaptchaField
+from crispy_forms.layout import Field
 from django import forms
 from django_summernote.widgets import SummernoteInplaceWidget
 from django.utils.translation import ugettext_lazy as _
@@ -6,6 +7,14 @@ from django.utils.translation import ugettext_lazy as _
 from bootstrap3_datetime.widgets import DateTimePicker
 from .models import Event, EventParticipation
 from .mixins import CrispyFormMixin, ReadOnlyFieldsMixin
+
+
+REGISTRATION_ENABLED_HELP_TEXT = _(
+    'Esta opción habilita la posiblidad de que la gente se inscriba al evento')
+HAS_SPONSORS_HELP_TEXT = _(
+    'Si se marca esta opción, y se habilitan las inscripciones, entonces se le consultará a los '
+    'inscriptos si quieren compartir sus datos con los sponsors y se les dará la opción de linkear '
+    'a un CV.')
 
 
 class EventForm(CrispyFormMixin):
@@ -34,7 +43,10 @@ class EventForm(CrispyFormMixin):
         )
     )
 
-    class Meta(CrispyFormMixin.Meta):
+    def get_crispy_fields(self):
+        return self.Meta.fields
+
+    class Meta:
         model = Event
         fields = (
             'name',
@@ -44,12 +56,13 @@ class EventForm(CrispyFormMixin):
             'url',
             'start_at',
             'end_at',
-            'registration_enabled'
+            'registration_enabled',
+            'has_sponsors'
         )
         crispy_fields = fields
         help_texts = {
-            'registration_enabled': _('Esta opción habilita la posiblidad de que la gente se '
-                                      'inscriba al evento'),
+            'registration_enabled': HAS_SPONSORS_HELP_TEXT,
+            'has_sponsors': HAS_SPONSORS_HELP_TEXT,
         }
 
     def clean(self):
@@ -71,17 +84,42 @@ class EventForm(CrispyFormMixin):
 class AnonymousEventParticipationForm(CrispyFormMixin):
     captcha = CaptchaField()
 
-    class Meta(CrispyFormMixin.Meta):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        event = self.initial['event']
+        if not event.has_sponsors:
+            for field_name in ('cv', 'share_with_sponsors'):
+                field_layout = self.helper[field_name]
+                field_layout.wrap(Field, type="hidden")
+
+    class Meta:
         model = EventParticipation
         fields = (
             'name',
             'email',
             'interest',
             'seniority',
+            'share_with_sponsors',
+            'cv',
             'captcha',
         )
-        crispy_fields = fields
+        help_texts = {
+            'interest': _('¿Qué temas de Python te interesaría ver en una charla'),
+            'cv': 'Dejanos un link a tu CV, perfil de LinkedIn o algo similar'
+        }
 
 
 class AuthenticatedEventParticipationForm(ReadOnlyFieldsMixin, AnonymousEventParticipationForm):
-    readonly_fields = ('name', 'email')
+    # The authenticated user does not need to fill a captcha. That's why the field is marked as
+    # read-only and not included in the fields list (so it won't render).
+    readonly_fields = ('name', 'email', 'captcha',)
+
+    class Meta(AnonymousEventParticipationForm.Meta):
+        fields = (
+            'name',
+            'email',
+            'interest',
+            'seniority',
+            'share_with_sponsors',
+            'cv',
+        )
