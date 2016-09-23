f4a6cbbfc6d2efdb7bddfd97ec652616ba871baf
Martín Gaitán <gaitan@gmail.com>
2016-09-23 02:28:28 -0300

    remove vagrant

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-aptitude update
-
-yes | aptitude install git-core redis-server python3-dev python3-pip gettext libxml2-dev libxslt1-dev zlib1g-dev
-
-pip3 install -r /vagrant/requirements.txt
