00d27a623096f15449b9e69324f62e282c2eb8aa
Carlos de la Torre <cmdelatorre@gmail.com>
2017-04-09 22:07:53 -0300

    Refactor the homepage as a class-based view

--- community/views.py
+++ community/views.py
@@ -6,6 +6,7 @@
 from django.db.models import F, Value, CharField
 from django.http import Http404
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.list import MultipleObjectMixin
 from django.utils.translation import ugettext_lazy as _
@@ -17,29 +18,53 @@ from news.models import NewsArticle
 RECENT_ITEMS_LEN = 10
 
 
-def homepage(request):
-    # Ensure news results have a 'body' field
-    news = NewsArticle.objects.order_by('-created')
-    news = news.annotate(description=F('body'),
-                         category=Value('Noticias', output_field=CharField()))
+# Auxiliary function for the homepage context
+def aux_qs_to_list_for_context(qs):
+    """Cast the given QuerySet to a list with at most RECENT_ITEMS_LEN elements"""
+    return list(qs[:RECENT_ITEMS_LEN])
 
-    jobs = Job.objects.order_by('-created')
-    jobs = jobs.annotate(category=Value('Trabajos', output_field=CharField()))
 
-    events = Event.objects.filter(start_at__lt=now())
-    events = events.order_by('-start_at')
-    # Ensure events results have 'created' and 'title' fields
-    events = events.annotate(created=F('start_at'),
-                             title=F('name'),
-                             category=Value('Eventos', output_field=CharField()))
+class HomePageView(TemplateView):
+    template_name = "community/index.html"
 
-    # Sort the last news, jobs and events to define the definitive 'recent' list
-    recent = (list(news[:RECENT_ITEMS_LEN]) +
-              list(jobs[:RECENT_ITEMS_LEN]) +
-              list(events[:RECENT_ITEMS_LEN]))
-    recent = sorted(recent, key=lambda r: r.created, reverse=True)[:RECENT_ITEMS_LEN]
+    def get_events_for_context(self):
+        """Ensure events have 'category', created' and 'title' fields"""
+        events = Event.objects.filter(start_at__lt=now())
+        events = events.order_by('-start_at')
+        events = events.annotate(
+            created=F('start_at'),
+            title=F('name'),
+            category=Value('Eventos', output_field=CharField()))
+        return aux_qs_to_list_for_context(events)
 
-    return render(request, 'community/index.html', {'recent': recent})
+    def get_jobs_for_context(self):
+        """Ensure jobs have a 'category' field"""
+        jobs = Job.objects.order_by('-created')
+        jobs = jobs.annotate(category=Value('Trabajos', output_field=CharField()))
+        return aux_qs_to_list_for_context(jobs)
+
+    def get_news_for_context(self):
+        """Ensure news have 'category' and 'description' fields"""
+        news = NewsArticle.objects.order_by('-created')
+        news = news.annotate(
+            description=F('body'),
+            category=Value('Noticias', output_field=CharField()))
+        return aux_qs_to_list_for_context(news)
+
+    def get_context_data(self, **kwargs):
+        """Add to the template context a list of the most recent events, jobs and news"""
+        news = self.get_news_for_context()
+        jobs = self.get_jobs_for_context()
+        events = self.get_events_for_context()
+        # Sort the last news, jobs and events to define the definitive 'recent' list
+        recent = sorted(news + jobs + events, key=lambda r: r.created, reverse=True)
+
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['recent'] = recent[:RECENT_ITEMS_LEN]
+        return context
+
+
+homepage = HomePageView.as_view()
 
 
 def learning(request):
