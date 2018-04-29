5712f7c5ada20fe08b9b83e10831410b93027081
Carlos Matías de la Torre <cdelatorre@machinalis.com>
2018-04-29 16:18:26 -0300

    split function to use ir

--- jobs/utils.py
+++ jobs/utils.py
@@ -1,15 +1,15 @@
 import unicodedata
 
 
-def normalize_tags(tags):
-    """
-        Parse a list of tags and removed duplicated tags and non valid chars
+def normalize(tag):
+    """Normalize a single tag: remove non valid chars, lower case all."""
 
-    """
-    results = set()
-    for tag in tags:
-        tag_stripped = tag.strip()
-        value = unicodedata.normalize("NFKD", tag_stripped.lower())
-        value = value.encode('ascii', 'ignore').decode('utf-8')
-        results.add(value)
-    return results
+    tag_stripped = tag.strip()
+    value = unicodedata.normalize("NFKD", tag_stripped.lower())
+    value = value.encode('ascii', 'ignore').decode('utf-8')
+    return value
+
+
+def normalize_tags(tags):
+    """Parse a list of tags and removed duplicated tags and non valid chars."""
+    return {normalize(tag) for tag in tags}
