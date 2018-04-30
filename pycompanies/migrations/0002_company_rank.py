0c7d2fc7b24582524d544280de6bf8128eb053ad
Eduardo Enriqez (eduzen) <eduardo.a.enriquez@gmail.com>
2018-04-30 17:38:47 -0300

    Changes after code review

--- pycompanies/migrations/0002_company_rank.py
+++ pycompanies/migrations/0002_company_rank.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='company',
             name='rank',
-            field=models.PositiveIntegerField(default=1),
+            field=models.PositiveIntegerField(default=0),
         ),
     ]
