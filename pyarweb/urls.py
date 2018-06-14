8782199a7e11417987bf3d8c3cbf3859eef3cdd7
Federico Jurio <fdjurio@gmail.com>
2018-06-14 11:44:32 -0300

    Issue-413: Depreco app tutoring

--- pyarweb/urls.py
+++ pyarweb/urls.py
@@ -26,8 +26,6 @@ urlpatterns = patterns(
     url(r'^irc/$', irc, name='irc'),
     url(r'^buscador/$', buscador, name='buscador'),
 
-    url(r'^tutoring/', include('tutoring.urls')),
-
     url(r'^$', 'community.views.homepage', name='homepage'),
     url(r'^aprendiendo-python/', 'community.views.learning', name='aprendiendo'),
     url(r'^nosotros/', 'community.views.about_pyar', name='about_pyar'),
