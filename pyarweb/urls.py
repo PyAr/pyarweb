dd330ce4a5fa42cbddc93c60abd1207c4ce4fe6d
Tomas Zulberti <tomas@jampp.com>
2020-05-31 12:14:59 -0300

    ISSUE-9: Remove waliki from URLs

--- pyarweb/urls.py
+++ pyarweb/urls.py
@@ -36,7 +36,6 @@ urlpatterns = patterns(
     url(r'^accounts/', include('allauth.urls')),
     url(r'^eventos/', include('events.urls', namespace='events')),
     url(r'^planeta/', include('planet.urls')),
-    url(r'^wiki/', include('waliki.urls')),
     url(r'^captcha/', include('captcha.urls')),
     url(r'^email_confirmation/', include('email_confirm_la.urls',
                                          namespace='email_confirm_la')),
