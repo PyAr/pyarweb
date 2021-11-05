897d8b5093f8fb0912304b1310a4d6b169d71b98
German Podesta <german.m.podesta@gmail.com>
2021-11-05 16:51:21 -0300

    Updated JobOffer's factory to include all the needed fields

--- joboffers/tests/factories.py
+++ joboffers/tests/factories.py
@@ -1,24 +1,30 @@
-from factory import Faker, SubFactory, Sequence, post_generation
+from factory import Faker, Sequence, SubFactory, fuzzy, post_generation
 from factory.django import DjangoModelFactory
 
-from pycompanies.tests.factories import CompanyFactory
 from events.tests.factories import UserFactory
-from joboffers.models import JobOffer
+from joboffers.models import Experience, HiringType, JobOffer, Remoteness
+from pycompanies.tests.factories import CompanyFactory
 
 
 class JobOfferFactory(DjangoModelFactory):
     class Meta:
         model = JobOffer
 
-    title = Sequence(lambda n: 'job_title_%i' % n)
+    title = Faker('job')
     company = SubFactory(CompanyFactory)
     location = Faker('address')
     contact_mail = Faker('email')
     contact_phone = Faker('phone_number')
     contact_url = Faker('url')
+    experience = fuzzy.FuzzyChoice(Experience.values)
+    remoteness = fuzzy.FuzzyChoice(Remoteness.values)
+    # tags
+    hiring_type = fuzzy.FuzzyChoice(HiringType.values)
+    salary = Faker('pricetag')
+    description = Faker('paragraph')
+
     created_by = SubFactory(UserFactory)
     modified_by = SubFactory(UserFactory)
-
     @post_generation
     def set_created(obj, create, extracted, **kwargs):
         """
