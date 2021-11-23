dc713d7ae05c9c5fe4c42fb4184b28f9c11bec47
Germán Podestá <german.m.podesta@gmail.com>
2021-11-23 18:29:19 -0300

    Formatted the tags like bootstrap's tags (joboffers)

--- joboffers/static/js/joboffers/search-form.js
+++ joboffers/static/js/joboffers/search-form.js
@@ -6,7 +6,13 @@ document.addEventListener('dal-init-function', function () {
     $(element).on('select2:selecting', function(event) {
       const data = event.params.args.data;
       const tagId = data.id
+
       $(`input[name="tags"][value="${tagId}"]`).prop('checked', true)
+
+      const inputHtml = `<input type="checkbox" name="tags" value="${tagId}" checked>`
+      const labelHtml = `<label class="label label-primary">${data.text} ${inputHtml}</label>`;
+
+      $('#id_tags').append($(labelHtml))
       $(element).select2('close')
       event.preventDefault()
     })
