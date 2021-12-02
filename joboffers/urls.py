3606f2b570b77234b8afcf0d0c2e2283fd8c8dac
German Podesta <german.m.podesta@gmail.com>
2021-12-02 12:51:26 -0300

    Added actions buttons to the joboffer state page

--- joboffers/urls.py
+++ joboffers/urls.py
@@ -1,14 +1,34 @@
 from django.contrib.auth.decorators import login_required
-from django.views.generic.detail import DetailView
 from django.urls import re_path
-from .models import JobOffer
-from .views import JobOfferAdminView, JobOfferCreateView, JobOfferUpdateView
+from .views import (
+  JobOfferAdminView, JobOfferCreateView, JobOfferDetailView, JobOfferUpdateView, JobOfferRejectView,
+  JobOfferAcceptView, JobOfferReactivateView
+)
 
 
 urlpatterns = [
     re_path(r'^admin/$', login_required(JobOfferAdminView.as_view()), name='admin'),
     re_path(r'^nueva/$', login_required(JobOfferCreateView.as_view()), name='add'),
-    re_path(r'^(?P<slug>[\w-]+)/$', DetailView.as_view(model=JobOffer), name='view'),
+    re_path(
+      r'^(?P<slug>[\w-]+)/rechazar/$', login_required(JobOfferRejectView.as_view()), name='reject'
+    ),
+    re_path(
+      r'^(?P<slug>[\w-]+)/aceptar/$', login_required(JobOfferAcceptView.as_view()), name='approve'
+    ),
+    re_path(
+      r'^(?P<slug>[\w-]+)/desactivar/$', login_required(JobOfferAcceptView.as_view()),
+      name='deactivate'
+    ),
+    re_path(
+      r'^(?P<slug>[\w-]+)/reactivar/$', login_required(JobOfferReactivateView.as_view()),
+      name='reactivate'
+    ),
+    re_path(
+      r'^(?P<slug>[\w-]+)/pedir-moderacion/$', login_required(JobOfferReactivateView.as_view()),
+      name='request_moderation'
+    ),
+
+    re_path(r'^(?P<slug>[\w-]+)/$', JobOfferDetailView.as_view(), name='view'),
     re_path(
       r'^(?P<slug>[\w-]+)/editar$', login_required(JobOfferUpdateView.as_view()),
       name='edit'
--- joboffers/views.py
+++ joboffers/views.py
@@ -1,9 +1,16 @@
+from django.shortcuts import reverse
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.utils.translation import gettext as _
+from django.views.generic import ListView, RedirectView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 
 from pycompanies.models import Company
 from .forms import JobOfferForm
+from .joboffer_actions import (
+    CODE_EDIT, CODE_REJECT, CODE_REACTIVATE, CODE_DEACTIVATE,
+    CODE_REQUEST_MODERATION, CODE_APPROVE
+)
 from .models import JobOffer
 
 
@@ -18,10 +25,74 @@ class JobOfferCreateView(CreateView):
         return super().form_valid(form)
 
 
+ACTION_BUTTONS = {
+    CODE_EDIT: {
+        "target_url": 'joboffers:edit',
+        "text": _('Editar'),
+        "css_classes": ["btn-success"],
+        "icon_class": "glyphicon-pencil"
+    },
+    CODE_REJECT: {
+        "target_url": 'joboffers:reject',
+        "text": _('Rechazar'),
+        "css_classes": ["btn-danger"],
+        "icon_class": "glyphicon-thumbs-down"
+    },
+    # CODE_COMMENT: {
+    #     "target_url": 'joboffers:edit',
+    #     "text": _('Comentar'),
+    #     "css_classes": ["btn-default"],
+    #     "icon_class": "glyphicon-comment"
+    # },
+    CODE_REACTIVATE: {
+        "target_url": 'joboffers:reactivate',
+        "text": _('Volver a Activar'),
+        "css_classes": ["btn-default"],
+        "icon_class": "glyphicon-arrow-up"
+    },
+    CODE_DEACTIVATE: {
+        "target_url": 'joboffers:deactivate',
+        "text": _('Desactivar'),
+        "css_classes": ["btn-warning"],
+        "icon_class": "glyphicon-minus-sign"
+    },
+    CODE_REQUEST_MODERATION: {
+        "target_url": 'joboffers:request_moderation',
+        "text": _('Solicitar Moderación'),
+        "css_classes": ["btn-secondary"],
+        "icon_class": "glyphicon-eye-open"
+    },
+    CODE_APPROVE: {
+        "target_url": 'joboffers:approve',
+        "text": _('Aprobar'),
+        "css_classes": ["btn-default"],
+        "icon_class": "glyphicon-pencil"
+    }
+}
+
+
+class JobOfferDetailView(DetailView):
+    model = JobOffer
+
+    def get_action_buttons(self):
+        # TODO: call get_valid_actions() instead
+        valid_actions = [CODE_EDIT, CODE_REJECT]
+
+        return [ACTION_BUTTONS[action_name] for action_name in valid_actions]
+
+    def get_context_data(self, object):
+        ctx = super().get_context_data()
+        ctx['action_buttons'] = self.get_action_buttons()
+        return ctx
+
+
 class JobOfferUpdateView(UpdateView):
     model = JobOffer
     form_class = JobOfferForm
-    success_url = reverse_lazy("joboffers:admin")
+    success_url = "joboffers:view"
+
+    def get_success_url(self, *args, **kwargs):
+        return reverse('joboffers:view', kwargs=self.kwargs)
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
@@ -44,3 +115,23 @@ class JobOfferAdminView(ListView):
         ctx = super().get_context_data()
         ctx['company'] = Company.objects.first()
         return ctx
+
+
+class JobOfferRejectView(RedirectView):
+    pattern_name = 'joboffers:view'
+
+
+class JobOfferAcceptView(RedirectView):
+    pattern_name = 'joboffers:view'
+
+
+class JobOfferReactivateView(RedirectView):
+    pattern_name = 'joboffers:view'
+
+
+class JobOfferDeactivateView(RedirectView):
+    pattern_name = 'joboffers:view'
+
+
+class JobOfferRequestModerationView(RedirectView):
+    pattern_name = 'joboffers:view'
