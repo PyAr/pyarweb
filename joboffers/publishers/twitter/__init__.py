a04b423c732fb20b47a2fbb9f0e8570bea8f781d
Juan Manuel Schillaci <jmschillaci@gmail.com>
2022-02-17 21:48:50 -0300

    Change string representation for logging

--- joboffers/publishers/twitter/__init__.py
+++ joboffers/publishers/twitter/__init__.py
@@ -5,44 +5,50 @@ from django.conf import settings
 
 from joboffers.publishers import Publisher
 
-ERROR_LOG_MESSAGE_AUTH = ('Falló al querer generar las siguientes credenciales para twitter'
-                          ' TWITTER_CONSUMER_KEY: %s'
-                          ' TWITTER_CONSUMER_SECRET: %s'
-                          ' TWITTER_ACCESS_TOKEN: %s'
-                          ' TWITTER_ACCESS_SECRET: %s'
-                          ' Error: %s')
 
-ERROR_LOG_MESSAGE_POST = ('Falló al querer twitear con las siguientes credenciales.'
-                          ' TWITTER_CONSUMER_KEY: %s'
-                          ' TWITTER_CONSUMER_SECRET: %s'
-                          ' TWITTER_ACCESS_TOKEN: %s'
-                          ' TWITTER_ACCESS_SECRET: %s'
-                          ' Error: %s')
+def _repr_credentials():
+    """Show a string representation of twitter credentials."""
+    # Need to convert to string, in case they are strings or they are not set
+    credentials_repr = (
+        f' TWITTER_CONSUMER_KEY: {settings.TWITTER_CONSUMER_KEY} '
+        f' TWITTER_CONSUMER_SECRET: {settings.TWITTER_CONSUMER_SECRET} '
+        f' TWITTER_ACCESS_TOKEN: {settings.TWITTER_ACCESS_TOKEN} '
+        f' TWITTER_ACCESS_SECRET: {settings.TWITTER_ACCESS_SECRET} '
+    )
+
+    return credentials_repr
+
+
+ERROR_LOG_MESSAGE_AUTH = (
+    'Falló al querer generar las siguientes credenciales para twitter' '%s Error: %s'
+)
+
+ERROR_LOG_MESSAGE_POST = (
+    'Falló al querer twitear con las siguientes credenciales.' '%s Error: %s'
+)
 
 ERROR_LOG_MESSAGE_GENERIC = 'Falló al querer publicar a twitter, data=%s: %s'
 
 
 class TwitterPublisher(Publisher):
     """Twitter Publisher."""
+
     name = 'Twitter'
 
     def _push_to_api(self, message: str):
         """Publish a message to twitter."""
 
         try:
-            auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY,
-                                       settings.TWITTER_CONSUMER_SECRET)
-            auth.set_access_token(settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_SECRET)
+            auth = tweepy.OAuthHandler(
+                settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET
+            )
+            auth.set_access_token(
+                settings.TWITTER_ACCESS_TOKEN, settings.TWITTER_ACCESS_SECRET
+            )
             api = tweepy.API(auth)
         except Exception as err:
             status = None
-            # Need to convert to string, in case they are strings or they are not set
-            logging.error(ERROR_LOG_MESSAGE_AUTH,
-                          str(settings.TWITTER_CONSUMER_KEY),
-                          str(settings.TWITTER_CONSUMER_SECRET),
-                          str(settings.TWITTER_ACCESS_TOKEN),
-                          str(settings.TWITTER_ACCESS_SECRET),
-                          err)
+            logging.error(ERROR_LOG_MESSAGE_AUTH, _repr_credentials(), err)
             return status
 
         try:
@@ -51,20 +57,10 @@ class TwitterPublisher(Publisher):
             # Specifically cacthing this exception as it could be helpful to debug errors on
             # this end.
             status = 401
-            logging.error(ERROR_LOG_MESSAGE_POST,
-                          settings.TWITTER_CONSUMER_KEY,
-                          settings.TWITTER_CONSUMER_SECRET,
-                          settings.TWITTER_ACCESS_TOKEN,
-                          settings.TWITTER_ACCESS_SECRET,
-                          err)
+            logging.error(ERROR_LOG_MESSAGE_POST, _repr_credentials(), err)
         except Exception as err:
             status = None
-            logging.error(ERROR_LOG_MESSAGE_POST,
-                          settings.TWITTER_CONSUMER_KEY,
-                          settings.TWITTER_CONSUMER_SECRET,
-                          settings.TWITTER_ACCESS_TOKEN,
-                          settings.TWITTER_ACCESS_SECRET,
-                          err)
+            logging.error(ERROR_LOG_MESSAGE_POST, _repr_credentials(), err)
         else:
             status = 200
 
--- joboffers/tests/test_twitter_publisher.py
+++ joboffers/tests/test_twitter_publisher.py
@@ -2,7 +2,9 @@ from unittest.mock import patch
 
 import tweepy
 
-from ..publishers.twitter import ERROR_LOG_MESSAGE_AUTH, ERROR_LOG_MESSAGE_POST, TwitterPublisher
+from ..publishers.twitter import (ERROR_LOG_MESSAGE_AUTH,
+                                  ERROR_LOG_MESSAGE_POST, TwitterPublisher,
+                                  _repr_credentials)
 
 DUMMY_MESSAGE = 'message'
 DUMMY_EXCEPTION_MESSAGE = 'Oops'
@@ -34,38 +36,28 @@ def test_push_to_api_wrong_credential_format(settings, caplog):
     settings.TWITTER_ACCESS_TOKEN = 123
     settings.TWITTER_ACCESS_SECRET = 123
     status = TwitterPublisher()._push_to_api('message')
-    expected_error_message = ERROR_LOG_MESSAGE_AUTH % (
-                                                        str(settings.TWITTER_CONSUMER_KEY),
-                                                        str(settings.TWITTER_CONSUMER_SECRET),
-                                                        str(settings.TWITTER_ACCESS_TOKEN),
-                                                        str(settings.TWITTER_ACCESS_SECRET),
-                                                        '')
+    expected_error_message = ERROR_LOG_MESSAGE_AUTH % (_repr_credentials(), '')
 
     assert expected_error_message in caplog.text
     assert status is None
 
 
 @patch(
-        'joboffers.publishers.twitter.tweepy.API',
-    )
+    'joboffers.publishers.twitter.tweepy.API',
+)
 def test_push_to_api_bad_credentials(mock_api, settings, caplog):
     """Test exception when the credentials are in the wrong format."""
     mock_api.return_value = DummyAPIBad
     status = TwitterPublisher()._push_to_api('message')
-    expected_error_message = ERROR_LOG_MESSAGE_POST % (
-                                                        settings.TWITTER_CONSUMER_KEY,
-                                                        settings.TWITTER_CONSUMER_SECRET,
-                                                        settings.TWITTER_ACCESS_TOKEN,
-                                                        settings.TWITTER_ACCESS_SECRET,
-                                                        '')
+    expected_error_message = ERROR_LOG_MESSAGE_POST % (_repr_credentials(), '')
 
     assert expected_error_message in caplog.text
     assert status == 401
 
 
 @patch(
-        'joboffers.publishers.twitter.tweepy.API',
-    )
+    'joboffers.publishers.twitter.tweepy.API',
+)
 def test_push_to_api_ok(mock_api):
     mock_api.return_value = DummyAPIOK
 
