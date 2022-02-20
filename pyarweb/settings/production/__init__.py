3bfd29d404668479ec333799d0c947a4fa5b20d8
Tomas Zulberti <tzulberti@gmail.com>
2022-02-20 19:31:04 +0800

    Merge changes to master (#497)
    * Removed planet app.
    
    * Updated everything.
    
    * Use a modern datepicker.
    
    * Removed email confirmation.
    
    * Use the static template function to load the CSS images (#489)
    
    * Use the static template function to load the CSS images
    
    * Fixed static and media images
    
    * Typo en el dominio de la wiki
    
    Co-authored-by: Facundo Batista <facundo@taniquetil.com.ar>

--- pyarweb/settings/production/__init__.py
+++ pyarweb/settings/production/__init__.py
@@ -3,6 +3,9 @@ import sys
 
 from pyarweb.settings.base import *  # NOQA
 
+DEBUG = False
+TEMPLATE_DEBUG = False
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
@@ -11,9 +14,6 @@ CACHES = {
 }
 
 ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = '/code/static2/'
-STATIC_URL = '/static/'
 
 ACCOUNT_EMAIL_SUBJECT_PREFIX = "[PyAr]"
 
@@ -27,6 +27,7 @@ EMAIL_TIMEOUT = int(os.environ.get('EMAIL_TIMEOUT', '10'))
 EMAIL_FROM = os.environ.get('EMAIL_FROM', 'do_not_reply@python.org.ar')
 
 PYAR_WIKI_URL = 'https://wiki.python.org.ar/'
+
 
 SENDFILE_BACKEND = "sendfile.backends.nginx"
 SENDFILE_ROOT = '/home/www-pyar/pyarweb/pyarweb/media/waliki_attachments/'
