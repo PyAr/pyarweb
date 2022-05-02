d3d9051b395cb05aa911a9c02b4a7d5d65e8a281
German Podesta <german.m.podesta@gmail.com>
2022-05-02 16:59:56 -0300

    Changed default publishers settings

--- pyarweb/settings/development/__init__.py
+++ pyarweb/settings/development/__init__.py
@@ -11,12 +11,7 @@ DEBUG = True
 # Prefix for telegram messages
 TELEGRAM_MESSAGE_PREFIX = '[DEV]'
 
-SOCIAL_NETWORKS_PUBLISHERS = [
-  'joboffers.publishers.discourse.DiscoursePublisher',
-  'joboffers.publishers.facebook.FacebookPublisher',
-  'joboffers.publishers.telegram.TelegramPublisher',
-  'joboffers.publishers.twitter.TwitterPublisher'
-]
+SOCIAL_NETWORKS_PUBLISHERS = []  # Empty by default to avoid sending on local testing
 
 # BASE_URL to use in any notification that might require them
 BASE_URL = 'http://localhost:8000'
