92afc775aa5bc93a2746113fe2b658325c17a7c3
German Podesta <german.m.podesta@gmail.com>
2022-06-10 14:18:55 -0300

    Added search field for UserCompanyProfile

--- pycompanies/admin.py
+++ pycompanies/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Company, UserCompanyProfile
 
 
+class UserCompanyProfileAdmin(admin.ModelAdmin):
+    search_fields = ('company__name', )
+
+
 admin.site.register(Company)
-admin.site.register(UserCompanyProfile)
+admin.site.register(UserCompanyProfile, UserCompanyProfileAdmin)
