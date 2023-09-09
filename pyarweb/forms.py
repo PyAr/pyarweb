de4d8457b35932c4b2ee7c72282549f319ce099a
Marco Richetta <marcorichetta@gmail.com>
2023-09-09 06:35:54 -0300

    serve a double resolution captcha

--- pyarweb/forms.py
+++ pyarweb/forms.py
@@ -6,6 +6,11 @@ class CustomCaptchaTextInput(CaptchaTextInput):
     # HACK - No entiendo por qué esto apunta a community/templates y no al /templates en el root
     template_name = "account/custom_captcha.html"
 
+    def image_url(self):
+        # Agrego el "@2" para enviar un captcha con más resolucioón
+        # https://django-simple-captcha.readthedocs.io/en/latest/advanced.html#captcha-2x-image
+        return super().image_url().removesuffix("/") + "@2"
+
 
 class SignupFormWithCaptcha(SignupForm):
     captcha = CaptchaField(widget=CustomCaptchaTextInput)
